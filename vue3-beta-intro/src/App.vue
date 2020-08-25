bbd638061c7371d28cc34c4fa44a53c0ad040b71
Mustafa Demir <mustafa.demir@kboxglobal.com>
2020-08-25 16:18:30 +0300

    vue 3 (beta) intro

---
+++ vue3-beta-intro/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+      {{ number }}<br><br>
+      {{  messageComputed }}<br><br>
+      <div>
+        <button @click="increase">Artir</button> |
+        <button @click="decrease">Azalt</button>
+      </div>
+    </div>
+</template>
+
+<script>
+    import { ref, onMounted, watch, computed } from 'vue';
+    export default {
+      name: 'App',
+      setup() {
+        // data
+        const number = ref(0);
+
+        //functions or methods
+        const increase = () => {
+          number.value++;
+        }
+        const decrease = () => {
+          number.value--;
+        }
+
+        //watch
+        watch(number, (newVal, oldVal) => {
+          console.log(`new value : ${newVal}`);
+          console.log(`old value : ${oldVal}`);
+          if (newVal == 10) {
+            number.value = 0;
+          }
+        })
+
+        //computed
+        const messageComputed = computed(()=> {
+          return `Sayı değeri ${number.value}`;
+        })
+
+        //mounted
+        onMounted( ()=> {
+          number.value = 5;
+        })
+
+        return {
+          number,
+          onMounted,
+          increase,
+          decrease,
+          watch,
+          messageComputed
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
